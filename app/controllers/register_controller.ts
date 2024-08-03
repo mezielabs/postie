cbb6bbc3b55434cbcb0aea30f7354b30b167edc4
Chimezie Enyinnaya <meziemichael@gmail.com>
2024-08-03 17:40:50 +0100

    complete

---
+++ app/controllers/register_controller.ts
@@ -0,0 +1,26 @@
+import User from '#models/user'
+import { RegisterValidator } from '#validators/register'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class RegisterController {
+  create({ view }: HttpContext) {
+    return view.render('pages/auth/register')
+  }
+
+  async store({ request, auth, session, response }: HttpContext) {
+    const payload = await request.validateUsing(RegisterValidator)
+
+    const user = await User.create(payload)
+
+    await auth.use('web').login(user)
+
+    session.flash({
+      notification: {
+        type: 'success',
+        message: 'Registration successful.',
+      },
+    })
+
+    return response.redirect('/')
+  }
+}
